31a622e654bd50eba0afc831c6c1bd9f471e50ca
Christopher Jung <cjung@csu.fullerton.edu>
2022-11-23 15:50:55 -0800

    Initial Commit

---
+++ Proactive/ProactiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProactiveApp.swift
+//  Proactive
+//
+//  Created by Christopher Jung on 11/23/22.
+//
+
+import SwiftUI
+
+@main
+struct ProactiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
